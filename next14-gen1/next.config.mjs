9a6f23792177c453f59dcbd5d3f0bffed79dcdfd
aleburbridge <aleburbridge@gmail.com>
2025-04-01 08:28:06 +0900

    fixed next gen 1 config

--- next14-gen1/next.config.mjs
+++ next14-gen1/next.config.mjs
@@ -1,10 +1,6 @@
 import BuilderDevTools from "@builder.io/dev-tools/next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverOptions: {
-    port: 3000 // or whatever port you want
-  }
-}
+const nextConfig = BuilderDevTools()({});
 
 export default nextConfig;
